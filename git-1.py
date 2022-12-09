25e0bbe545c25f5104e9d918f1190ccb9e9afa86
олексий <voisiklol@gamil.com>
2022-12-09 15:53:02 +0500

    привет питон

--- git-1.py
+++ git-1.py
@@ -1,6 +1,13 @@
+def my_superfunction():
+    print('Hello python!')
+
+
 def main():
     print('My first git program')
-    print('yep')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
